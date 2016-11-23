098968665661288c5b1d1c09b761193747c028b2
tnuma <tnuma@users.noreply.github.com>
2016-11-23 13:44:12 +0900

    Modified utility methods

--- src/com/numalab/java/mysample/Main.java
+++ src/com/numalab/java/mysample/Main.java
@@ -1,5 +1,7 @@
 package com.numalab.java.mysample;
 
+import com.numalab.java.mysample.algorithms.*;
+
 import java.text.*;
 import java.util.*;
 import java.util.stream.*;
@@ -10,11 +12,17 @@ public class Main {
 
     }
 
-    private static Stream<Integer> readSizeAndArrayAsStream() {
+    private static LongStream readLongArrayAsStream(int size) {
+        Scanner in = new Scanner(System.in);
+        return LongStream.range(0, size)
+                .map(i -> in.nextLong());
+    }
+
+    private static IntStream readSizeAndArrayAsStream() {
         Scanner in = new Scanner(System.in);
         int size = in.nextInt();
         return IntStream.range(0, size)
-                .mapToObj(i -> in.nextInt());
+                .map(i -> in.nextInt());
     }
 
 }
