af7c5510b222117ab817ee27d4e5a4288956669f
tnuma <tnuma@users.noreply.github.com>
2016-11-23 13:45:17 +0900

    Changed class structure

--- src/com/numalab/java/mysample/algorithms/Warmup.java
+++ src/com/numalab/java/mysample/algorithms/Warmup.java
@@ -5,121 +5,122 @@ import java.util.*;
 import java.util.stream.*;
 
 public class Warmup {
-}
 
-class CirculerArrayRotation {
-    public static void solve() throws Exception {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int k = in.nextInt();
-        int q = in.nextInt();
-        int[] a = new int[n];
-        for(int a_i=0; a_i < n; a_i++){
-            a[a_i] = in.nextInt();
-        }
-        for(int a0 = 0; a0 < q; a0++){
-            int m = in.nextInt();
-            System.out.println(a[(((m - k) % n) + n) % n]);
-        }
-    }
-}
-
-class TimeConversion {
-    public static void solve() throws Exception {
-        Scanner in = new Scanner(System.in);
-        String dateStr = in.nextLine();
-        SimpleDateFormat sdfIn = new SimpleDateFormat("hh:mm:ssa", Locale.US);
-        Date date = sdfIn.parse(dateStr);
-        SimpleDateFormat sdfOut = new SimpleDateFormat("HH:mm:ss");
-        System.out.println(sdfOut.format(date));
-    }
-}
-
-class Staircase {
-    public static void solve() throws Exception {
-        Scanner in = new Scanner(System.in);
-        int size = in.nextInt();
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                System.out.print(((i + j + 1) < size)
-                        ? ' '
-                        : '#');
+    public static class CirculerArrayRotation {
+        public static void main() throws Exception {
+            Scanner in = new Scanner(System.in);
+            int n = in.nextInt();
+            int k = in.nextInt();
+            int q = in.nextInt();
+            int[] a = new int[n];
+            for(int a_i=0; a_i < n; a_i++){
+                a[a_i] = in.nextInt();
             }
-            System.out.println();
-        }
-    }
-}
-
-class CompareTheTriplets {
-    public static void solve() {
-        Scanner in = new Scanner(System.in);
-        Rating aliceRating = new Rating();
-        Rating bobRating = new Rating();
-
-        aliceRating.readData(in);
-        bobRating.readData(in);
-
-        compareRating(aliceRating, bobRating);
-
-        System.out.println(aliceRating.score + " " + bobRating.score);
-    }
-
-    private static void compareRating(Rating r1, Rating r2)
-    {
-        for (int i = 0; i < r1.dataArray.length; i++) {
-            if (r1.dataArray[i] > r2.dataArray[i]) {
-                r1.score++;
-            } else if (r1.dataArray[i] < r2.dataArray[i]) {
-                r2.score++;
+            for(int a0 = 0; a0 < q; a0++){
+                int m = in.nextInt();
+                System.out.println(a[(((m - k) % n) + n) % n]);
             }
         }
     }
 
-    static class Rating {
-        private static final int COUNT = 3;
-        public int[] dataArray = new int[COUNT];
-        public int score;
+    public static class TimeConversion {
+        public static void main() throws Exception {
+            Scanner in = new Scanner(System.in);
+            String dateStr = in.nextLine();
+            SimpleDateFormat sdfIn = new SimpleDateFormat("hh:mm:ssa", Locale.US);
+            Date date = sdfIn.parse(dateStr);
+            SimpleDateFormat sdfOut = new SimpleDateFormat("HH:mm:ss");
+            System.out.println(sdfOut.format(date));
+        }
+    }
 
-        public void readData(Scanner sc) {
-            for (int i = 0; i < COUNT; i++) {
-                dataArray[i] = sc.nextInt();
+    public static class Staircase {
+        public static void main() throws Exception {
+            Scanner in = new Scanner(System.in);
+            int size = in.nextInt();
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    System.out.print(((i + j + 1) < size)
+                            ? ' '
+                            : '#');
+                }
+                System.out.println();
             }
         }
     }
-}
 
-class DiagonalDifference {
-    public static void solve() {
-        Scanner in = new Scanner(System.in);
-        int size;
-        size = in.nextInt();
-        int sum = 0;
-        for (int i = 0; i < size; i++) {
-            int[] row = readRow(in, size);
-            sum += row[i] - row[size - i - 1];
+    public static class CompareTheTriplets {
+        public static void main() {
+            Scanner in = new Scanner(System.in);
+            Rating aliceRating = new Rating();
+            Rating bobRating = new Rating();
+
+            aliceRating.readData(in);
+            bobRating.readData(in);
+
+            compareRating(aliceRating, bobRating);
+
+            System.out.println(aliceRating.score + " " + bobRating.score);
         }
-        System.out.println(Math.abs(sum));
-    }
 
-    private static int[] readRow(Scanner sc, int count) {
-        int[] result = new int[count];
-        for (int i = 0; i < count; i++) {
-            result[i] = sc.nextInt();
+        private static void compareRating(Rating r1, Rating r2)
+        {
+            for (int i = 0; i < r1.dataArray.length; i++) {
+                if (r1.dataArray[i] > r2.dataArray[i]) {
+                    r1.score++;
+                } else if (r1.dataArray[i] < r2.dataArray[i]) {
+                    r2.score++;
+                }
+            }
         }
-        return result;
-    }
-}
 
-class SimpleArraySum {
-    public static void solve() {
-        Optional<Integer> sum = readSizeAndArrayAsStream().reduce((s, i) -> s + i);
-        System.out.println(sum.orElse(0));
+        static class Rating {
+            private static final int COUNT = 3;
+            public int[] dataArray = new int[COUNT];
+            public int score;
+
+            public void readData(Scanner sc) {
+                for (int i = 0; i < COUNT; i++) {
+                    dataArray[i] = sc.nextInt();
+                }
+            }
+        }
     }
 
-    private static Stream<Integer> readSizeAndArrayAsStream() {
-        Scanner in = new Scanner(System.in);
-        int size = in.nextInt();
-        return IntStream.range(0, size)
-                .mapToObj(i -> in.nextInt());
+    public static class DiagonalDifference {
+        public static void main() {
+            Scanner in = new Scanner(System.in);
+            int size;
+            size = in.nextInt();
+            int sum = 0;
+            for (int i = 0; i < size; i++) {
+                int[] row = readRow(in, size);
+                sum += row[i] - row[size - i - 1];
+            }
+            System.out.println(Math.abs(sum));
+        }
+
+        private static int[] readRow(Scanner sc, int count) {
+            int[] result = new int[count];
+            for (int i = 0; i < count; i++) {
+                result[i] = sc.nextInt();
+            }
+            return result;
+        }
     }
+
+    public static class SimpleArraySum {
+        public static void main() {
+            Optional<Integer> sum = readSizeAndArrayAsStream().reduce((s, i) -> s + i);
+            System.out.println(sum.orElse(0));
+        }
+
+        private static Stream<Integer> readSizeAndArrayAsStream() {
+            Scanner in = new Scanner(System.in);
+            int size = in.nextInt();
+            return IntStream.range(0, size)
+                    .mapToObj(i -> in.nextInt());
+        }
+    }
+
 }
